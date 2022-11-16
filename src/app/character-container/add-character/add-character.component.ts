035fc01f5b7ed4162934b9c8dd913fbc8b5873ee
Felipe Barcon <felipe.barcon@gmail.com>
2022-11-16 23:16:28 +0100

    input added to character work on child add-character

--- src/app/character-container/add-character/add-character.component.ts
+++ src/app/character-container/add-character/add-character.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-add-character',
   templateUrl: './add-character.component.html',
   styleUrls: ['./add-character.component.css'],
 })
-export class AddCharacterComponent {
+export class AddCharacterComponent implements OnInit {
+  input: string = '';
+  characters: string[] = [];
+
   constructor() {}
+
+  addInput(input: string) {
+    this.characters.push(input);
+    this.input = '';
+    console.log(this.characters);
+  }
+
+  ngOnInit(): void {}
 }
